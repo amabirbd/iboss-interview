2b8c32fcdc43e8efeeca4e4b67d66e0c5b23539d
abir <abir4u2011@gmail.com>
2021-05-26 15:41:29 +0600

    first commit

---
+++ src/components/InpForm.js
@@ -0,0 +1,94 @@
+import React from "react";
+import { useFormik } from "formik";
+import { addUser, deleteUser } from "../actions/user";
+
+//redux
+import { useDispatch, useSelector } from "react-redux";
+
+import Table from "./Table";
+
+function InpForm() {
+  const dispatch = useDispatch();
+
+  const users = useSelector((state) => state.user);
+
+  const formik = useFormik({
+    initialValues: {
+      name: "",
+      email: "",
+    },
+    onSubmit: (values) => {
+      const _email = values?.email;
+      const found = users.find((user) => user?.email === _email);
+      if (found) {
+        // raise formik error
+        // show error message
+        return;
+      }
+      dispatch(addUser(values));
+      formik.resetForm();
+    },
+  });
+
+  return (
+    <>
+      <form onSubmit={formik.handleSubmit}>
+        <input
+          id="name"
+          name="name"
+          type="text"
+          placeholder="name"
+          onChange={formik.handleChange}
+          value={formik.values.name}
+          required
+        />
+        <input
+          id="email"
+          name="email"
+          type="email"
+          placeholder="email"
+          onChange={formik.handleChange}
+          value={formik.values.email}
+          required
+        />
+
+        <button type="submit">Submit</button>
+      </form>
+
+      <div className="show_data">
+        <h3>Users</h3>
+        <table className="user-table">
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map((user, index) => (
+              <tr key={index}>
+                <td>{user.name}</td>
+                <td>{user.email}</td>
+                <td
+                  className="delete_btn"
+                  onClick={() => {
+                    dispatch(deleteUser(index));
+                  }}
+                >
+                  Delete
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+      <div>
+        <h3>Table Data Set Example</h3>
+        <Table />
+      </div>
+    </>
+  );
+}
+
+export default InpForm;
---
+++ src/reducers/moduleReducer.js
@@ -0,0 +1,60 @@
+const initialState = [
+  {
+    id: 1,
+    moduleName: "Configuration",
+    activityName: "Item Category",
+    isCreate: false,
+    isView: true,
+    isEdit: true,
+    isDelete: false,
+    isApprove: false,
+  },
+  {
+    id: 2,
+    moduleName: "Configuration",
+    activityName: "Item",
+    isCreate: true,
+    isView: true,
+    isEdit: false,
+    isDelete: true,
+    isApprove: false,
+  },
+  {
+    id: 3,
+    moduleName: "Configuration",
+    activityName: "Item Category",
+    isCreate: false,
+    isView: true,
+    isEdit: false,
+    isDelete: true,
+    isApprove: false,
+  },
+  {
+    id: 4,
+    moduleName: "Configuration",
+    activityName: "Item",
+    isCreate: true,
+    isView: false,
+    isEdit: true,
+    isDelete: true,
+    isApprove: false,
+  },
+];
+
+const moduleReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "UPDATE_MODULE":
+      const newModule = action.payload;
+      const newState = state.map((item) => {
+        if (item?.id === newModule?.id) {
+          return newModule;
+        }
+        return item;
+      });
+      return newState;
+    default:
+      return state;
+  }
+};
+
+export default moduleReducer;
